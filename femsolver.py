04e41188579f604c3f4280c5682b97ab1fecff17
Ottar Hellan <37851628+ottarph@users.noreply.github.com>
2020-10-04 22:22:28 +0200

    Added linear system builder and solver

--- femsolver.py
+++ femsolver.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gauss_quadrature import *
 
+
 class Femsolver:
     """
         Finite element solver for 1D elliptic pde of the form
@@ -14,6 +15,7 @@ class Femsolver:
 
         """ Load function """
         self.f = f
+
         """ Dirichlet boundary conditions """
         self.u_1 = u_1
         self.u_2 = u_2
@@ -37,33 +39,113 @@ class Femsolver:
 
         """ Degree of polynomial interpolant """
         self.p = None
-        """ Number of elements """
+        """ Number of nodes """
         self.N = None
+        """ Number of elements """
+        self.K = None
+
+        """ Gauss-Legendre quadrature sample points, set for now. """
+        self.quad_samples = 5
 
         """ Triangulation """
         self.nodes = None
         self.edge_nodes = None
         self.elements = None
+        self.h = None
 
 
         """ Stiffness matrix """
         self.A = None
         """ Load vector """
         self.F = None
+        """ Solution vector """
+        self.u = None
 
         return
 
 
     def build_triangulation(self, N):
+        """ N+2 nodes, the edge nodes x_0, x_N+1 and N internal nodes x_1,...,x_N """
         self.nodes = np.linspace(self.a, self.b, N+2)
         self.edge_nodes = [self.nodes[0], self.nodes[-1]]
+        """ For 1D, K = N+1 elements in the triangulation """
         self.elements = [[self.nodes[i-1], self.nodes[i]] for i in range(1, N+2)]
 
-    def build_stiffness_matrix(self, p, N):
-        pass
+        self.h = self.nodes[1:] - self.nodes[:-1]
 
-    def build_load_vector(self, p, N):
-        pass
+        self.N = N
+        self.K = N+1
+
+        return
+
+
+    def build_stiffness_matrix(self, p):
+
+        if self.elements == None:
+            raise Exception("Require triangulation before building stiffness matrix.")
+        
+        self.p = p
+
+        self.A = np.zeros((self.N+2,self.N+2), dtype=float)
+
+        if p == 1:
+            Ke = self.Ke_1
+
+            """ For each element in triangulation """
+            for i in range(self.K):
+                self.A[i:i+2,i:i+2] += self.h[i] * Ke
+            
+            self.A = self.A[1:-1,1:-1]
+
+        elif p == 2:
+            raise NotImplementedError
+        
+        return
+
+
+    def build_load_vector(self, p):
+        
+        if self.elements == None:
+            raise Exception("Require triangulation before building stiffness matrix.")
+        
+        self.F = np.zeros(self.N+2)
+
+        if p == 1:
+
+            """ For each element in triangulation """
+            for i in range(self.K):
+                x0, x1 = self.elements[i]
+                hi = self.h[i]
+                g = lambda x: self.f(x0 + hi * x)
+                
+                Me1 = gauss_quad(lambda x: (1 - x) * g(x), 0, 1, self.quad_samples)
+                Me2 = gauss_quad(lambda x: x * g(x), 0, 1, self.quad_samples)
+                Me = np.array([Me1, Me2], dtype=float)
+
+                self.F[i:i+2] += hi * Me
+
+            self.F[1] -= self.h[0] * self.u_1
+            self.F[self.N] -= self.h[self.N] * self.u_2
+
+            self.F = self.F[1:-1]
+
+        elif p == 2:
+            raise NotImplementedError
+
+        return
+
+    def solve_linear_system(self):
+        
+        self.u = np.linalg.solve(self.A, self.F)
+
+        return
+
+    def plot_solution(self):
+
+
+
+
+
 
 
 def main():
@@ -79,8 +161,14 @@ def main():
     femsolver = Femsolver(sigma, f, a, b, u_1, u_2)
 
     N = 4
+    p = 1
     femsolver.build_triangulation(N)
-    print(femsolver.elements)
+    femsolver.build_stiffness_matrix(p)
+    print(femsolver.A)
+    femsolver.build_load_vector(p)
+    print(femsolver.F)
+    femsolver.solve_linear_system()
+    print(femsolver.u)
 
     return
 
